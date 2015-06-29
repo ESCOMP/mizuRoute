23d62ce0c390eed345a0675594179ee695da7e02
Bart Nijssen <nijssen@uw.edu>
2015-06-29 14:56:38 -0700

    Revert "1st version of NCAR/RAL routing tool - mizuRoute"

--- route.v1/build/gammln_s__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Jun 24 10:37:36 2015
-        MODULE GAMMLN_S__genmod
-          INTERFACE 
-            FUNCTION GAMMLN_S(XX)
-              REAL(KIND=4), INTENT(IN) :: XX
-              REAL(KIND=4) :: GAMMLN_S
-            END FUNCTION GAMMLN_S
-          END INTERFACE 
-        END MODULE GAMMLN_S__genmod
