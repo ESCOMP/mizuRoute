390b7a98b74b78b68efc0b9860a55c133a0e4503
Naoki Mizukami <mizukami@uncertainty.rap.ucar.edu>
2017-11-01 14:37:26 -0600

    clean up based on Joes suggestions

--- scripts/Calc_hru_wgtavg_nc.py
+++ scripts/Calc_hru_wgtavg_nc.py
@@ -18,14 +18,17 @@ import netCDF4 as nc4
 #         hardcoded variables              #
 ############################################
 # number of processors (= number of chunks of hru to be processed)
-NPROC=4
-# Name of netCDF variable for polygon ID in input data
-HDIM='hru_id'
-TDIM='Time'
-# Name of netCDF variable for weight in weight data
+NPROC=2
+# Name of variables in runoff netCDF
+GRID_ID_NAME='hru_id'
+TIME_VAR_NAME='time'
+# Name of variables in weight netCDF
+HRU_ID_NAME='hruid'
 WGTNM='weight'
 OVRPLYNM='overlapPolyId'
 OVRNM='overlaps'
+
+FILL_VALUE=-9999.0
 ############################################ 
 #              Class                       #
 ############################################
@@ -37,104 +40,90 @@ class wgtnc:
 
     def getWgtHru(self,hru):
         """For given hru id, get weight of the intersected polygons and associated ID and lat/lon"""
-        wgtAll        = getNetCDFData(self.ncName, WGTNM) 
-        overlapsIdAll = getNetCDFData(self.ncName, OVRPLYNM) 
-        overlapsAll   = getNetCDFData(self.ncName, OVRNM)
+        wgtAll        = get_netCDF_data(self.ncName, WGTNM) 
+        overlapsIdAll = get_netCDF_data(self.ncName, OVRPLYNM) 
 
-        self.hruList = self.getHruID()            # get hru id list
-        idx=self.hruList.index(hru)               # get indix in array corresponding hru
-        self.wgt        = np.asarray(list(wgtAll[idx]))       # Get overlapping poly's wgt list for hru
-        self.overlapsId = np.asarray(list(overlapsIdAll[idx])) # Get overlapping poly's wgt list for hru
-        self.overlaps   = overlapsAll[idx]        # Get number of overlapping polys
-        return (self.wgt, self.overlapsId, self.overlaps)
+        self.hruList = self.getHruID()
+        idx=self.hruList.index(hru)               # indix of corresponding hru
+        self.wgt = np.asarray(list(wgtAll[idx]))       # list of areal weight of overlapping poly for hru
+        self.overlap_ids = np.asarray(list(overlapsIdAll[idx])) # list of IDs of overlapping poly for hru
+        self.overlap_ids=self.overlap_ids[~np.isnan(self.overlap_ids)]
+        self.wgt=self.wgt[~np.isnan(self.wgt)]
 
-    def getHruIdName(self):
-        """ get Name of hru ID 
-            it must be the 1st dimension """
-        f = nc4.Dataset(self.ncName,'r')
-        self.dim = f.dimensions
-        self.dimName = self.dim.keys()
-        self.hruIdName = self.dimName[0]
-        return self.hruIdName
-        
+        return (self.wgt, self.overlap_ids)
+
     def getHruID(self):
         """ get hru ID list of basin"""
-        self.HruIdName=self.getHruIdName()
-        self.hruid = list(getNetCDFData(self.ncName, self.HruIdName))
+        self.hruid = list(get_netCDF_data(self.ncName, HRU_ID_NAME))
         return self.hruid
 
 ############################################
 #            Modules                       #
 ############################################
-def getNetCDFData(fn, varname):
+def get_netCDF_data(fn, varname):
     """Read <varname> variables from NetCDF <fn> """
     f = nc4.Dataset(fn,'r')
     data = f.variables[varname][:]
     f.close()
     return data
 
-def getNetCDFAtt(fn, varname,attName):
+def get_netCDF_attr(fn, varname,attName):
     """Read attribute of <varname> variables from NetCDF <fn> """
     f = nc4.Dataset(fn,'r')
     var = f.variables[varname]
     attData = getattr(var,attName)
     return attData
 
-def compAvgVal(nc_wgt, nc_in, varname, chunk):
-  """Compute areal weighted avg value of <varname> in <nc_in> for each hru based on hru's weight in <nc_wgt>""" 
-  wgt       = wgtnc(nc_wgt)       #instantaneous of wgtnc object
-  hruIDs    = wgt.getHruID()      #get hruID list 
-  hrudata   = split(hruIDs,NPROC)
-  hrulist   = hrudata[chunk]
+def comp_agv_val(nc_wgt, nc_in, varname, chunk):
+    """Compute areal weighted avg value of <varname> in <nc_in> for each hru based on hru's weight in <nc_wgt>""" 
+    wgt       = wgtnc(nc_wgt)
+    hruIDs    = wgt.getHruID()
+    hrudata   = split(hruIDs,NPROC)
+    hrulist   = hrudata[chunk]
 
-  dataVal  = getNetCDFData(nc_in,varname) # Get data value 
-  FillVal  = getNetCDFAtt(nc_in,varname,'_FillValue') # Get data value 
-  ghruid   = getNetCDFData(nc_in,HDIM)
-  dim1size = dataVal.shape[0]
-  #Initialize wgtVal[ntime,nhru] 
-  wgtVal = np.ones((dim1size,len(hrulist)))*-9999.0
-  #Loop through each hru polygon 
-  for i in range(len(hrulist)):
-    print hrulist[i]
-    # Get list of wgt for corresponding hru
-    (wgtArray, overlapsId, overlaps)=wgt.getWgtHru(hrulist[i])
-    #Count missing cells (No values in original input polygon) and valid cells
-    numvoid = 0
-    # Go through wgt list and replace value with zero for following cases
-    # where overlapping polygon has missing value - case1
-    # where overlapping polygon is outside nc_wgt domain - case2
-    a = np.zeros( (dim1size,len(wgtArray)) )
-    for j in range(overlaps):                     # Go through each overlapping polygon
-      # find index of grid cell that match up with hru id of overlapsId[j]
-      ij=np.column_stack(np.where(ghruid==overlapsId[j]))
-      # if nc_in netCDF does not cover nc_wgt domain - case2
-      if len(ij[0]) == 0:
-        numvoid+=1
-        wgtArray[j]=0.0
-      else:
-        a[:,j] = dataVal[:,ij[0,0],ij[0,1]]
-        #if value of overlapping polygon is missing data -case1
-        if any(a[:,j]==FillVal):
-          numvoid+=1
-          wgtArray[j] = 0.0
-    if np.nansum(wgtArray) > 0.0:
-      # Adjust weight value if valid weight value (> 0) exist in list
-      wgtArray = wgtArray/np.nansum(wgtArray)
-      wgtArray = np.where(np.isnan(wgtArray),0,wgtArray)
-      wgtVal[:,i]=np.dot(a,wgtArray.T).T
+    dataVal  = get_netCDF_data(nc_in,varname)
+    FillVal  = get_netCDF_attr(nc_in,varname,'_FillValue')
+    ghruid   = get_netCDF_data(nc_in,GRID_ID_NAME)
+    dim1size = dataVal.shape[0]
+    #Initialize wgt_ave_val[ntime,nhru] 
+    wgt_ave_val = np.full((dim1size,len(hrulist)),FILL_VALUE)
+    for i in range(len(hrulist)):
+        print hrulist[i] # Get list of wgt for corresponding hru
+        (wgt_array, overlap_ids)=wgt.getWgtHru(hrulist[i])
+        # Go through wgt list and replace value with zero for following cases
+        # where overlapping polygon has missing value - case1
+        # where overlapping polygon is outside nc_wgt domain - case2
+        a = np.zeros((dim1size,len(wgt_array)))
+        for j,overlap_id in enumerate(overlap_ids):
+            # find index of grid cell that match up with hru id of overlap_ids
+            row,col=np.where(ghruid==overlap_id)
+            # if nc_in netCDF does not cover nc_wgt domain - case2
+            if not (np.size(row) and np.size(col)):
+                wgt_array[j]=0.0
+            else:
+                a[:,j] = np.squeeze(dataVal[:,row,col])
+                #if value of overlapping polygon is missing data -case1
+                if any(a[:,j]==FillVal):
+                    wgt_array[j] = 0.0
+        sum_wgt=np.sum(wgt_array)
+        if sum_wgt > 0.0:
+            # Adjust weight value if valid weight value (> 0) exist in list
+            wgt_array = wgt_array/sum_wgt
+            wgt_array = np.where(np.isnan(wgt_array),0,wgt_array)
+            wgt_ave_val[:,i] = np.dot(a,wgt_array.T).T
 
-  return wgtVal
+    return wgt_ave_val
 
 def split(hrulist,n):
-  subsize=len(hrulist)/n
-  idx1=0
-  hrudata=[]
-  for i in range(n-1):
-    idx2=idx1+subsize
-    hrudata.append(hrulist[idx1:idx2])
-    idx1=idx2
-  hrudata.append(hrulist[idx1:])
-  return hrudata
+    subsize=len(hrulist)/n
+    idx1=0
+    hrudata=[]
+    for _ in range(n-1):
+        idx2=idx1+subsize
+        hrudata.append(hrulist[idx1:idx2])
+        idx1=idx2
+    hrudata.append(hrulist[idx1:])
+    return hrudata
 
 ############################################
 #                Main                      #
@@ -154,8 +143,6 @@ if __name__ == '__main__':
         usage()
 
     verbose = False
-    grid_info = False
-    proj_info = True
     for (opt,val) in opts:
         if opt == '-h':
             usage()
@@ -166,27 +153,26 @@ if __name__ == '__main__':
             usage()
 
     if len(args) == 4:
-      # Read three argument
-      nc_wgt  = args[0]
-      nc_in   = args[1]  
-      varname = args[2]  
-      nc_out  = args[3]
+        nc_wgt = args[0]
+        nc_in = args[1]  
+        varname = args[2]  
+        nc_out = args[3]
 
-      func=partial(compAvgVal, nc_wgt, nc_in, varname)
+        func=partial(comp_agv_val, nc_wgt, nc_in, varname)
 
-      pool = mp.Pool(processes=NPROC,maxtasksperchild=1)
-      results=pool.map(func,range(NPROC))
-      result=np.column_stack(results)
+        pool = mp.Pool(processes=NPROC,maxtasksperchild=1)
+        results = pool.map(func,range(NPROC))
+        result = np.column_stack(results)
 
-      with xr.open_dataset(nc_in) as ds:
-        timedata  = ds.coords[TDIM]     # Get time coordinate variable
-      with xr.open_dataset(nc_wgt) as ds:
-        hrudata = ds[HDIM].values
+        with xr.open_dataset(nc_in) as ds:
+            timedata = ds[TIME_VAR_NAME]
+        with xr.open_dataset(nc_wgt) as ds:
+            hrudata = ds[HRU_ID_NAME]
 
-      encoding={varname: {'dtype': 'float32', '_FillValue': -9999.0}}
-      foo = xr.DataArray(result, coords=[('time',timedata), ('hru',hrudata)], name=varname)
-      foo.encoding = encoding[varname]
-      foo.to_netcdf('test.nc')
+        encoding={varname: {'dtype':'float32', '_FillValue':FILL_VALUE}}
+        foo = xr.DataArray(result, coords=[('time',timedata), ('hru',hrudata)], name=varname)
+        foo.encoding = encoding[varname]
+        foo.to_netcdf(nc_out)
     else:
-      usage()
+        usage()
 
