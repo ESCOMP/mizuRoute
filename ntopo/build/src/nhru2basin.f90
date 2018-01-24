a2f18d470caa15f70a31f2baf65294e389284e1f
Martyn Clark <mclark@ucar.edu>
2018-01-24 15:39:41 -0700

    modify the mapping data structure to include the HRU counter

--- ntopo/build/src/nhru2basin.f90
+++ ntopo/build/src/nhru2basin.f90
@@ -7,19 +7,20 @@ MODULE nhru2basin
  ! --------------------------------------------------------------------------------------
  ! describe individual HRUs within a given basin
  TYPE IPOINT
-  INTEGER(I4B)                           :: hru_ix   ! index of HRU in the nHRU vector
-  INTEGER(I4B)                           :: hru_id   ! HRU id
-  REAL(DP)                               :: hru_lon  ! longitude
-  REAL(DP)                               :: hru_lat  ! latitude
-  REAL(DP)                               :: hru_elev ! elevation
-  REAL(DP)                               :: hru_area ! area
-  REAL(DP)                               :: wght     ! weight assigned to the grid point
+  INTEGER(I4B)                               :: hru_ix   ! index of HRU in the nHRU vector
+  INTEGER(I4B)                               :: hru_id   ! HRU id
+  REAL(DP)                                   :: hru_lon  ! longitude
+  REAL(DP)                                   :: hru_lat  ! latitude
+  REAL(DP)                                   :: hru_elev ! elevation
+  REAL(DP)                                   :: hru_area ! area
+  REAL(DP)                                   :: wght     ! weight assigned to the grid point
  END TYPE IPOINT
  ! Collection of HRUs assigned to a given basin
  TYPE ALL_POINTS
-  TYPE(IPOINT), DIMENSION(:), POINTER    :: cHRU
+  INTEGER(I4B)                               :: nHRU
+  TYPE(IPOINT), DIMENSION(:), ALLOCATABLE    :: cHRU
  END TYPE ALL_POINTS
  ! the nhru2basin correspondence
- TYPE(ALL_POINTS), DIMENSION(:), POINTER :: h2b
+ TYPE(ALL_POINTS), DIMENSION(:), ALLOCATABLE :: h2b
  ! --------------------------------------------------------------------------------------
 END MODULE nhru2basin
