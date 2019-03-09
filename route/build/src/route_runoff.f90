5a8936ccb5351a33f66793934e2172e60e55dc7c
Naoki Mizukami <mizukami@ucar.edu>
2019-03-09 07:23:09 -0700

    changed due to the other subroutines

--- route/build/src/route_runoff.f90
+++ route/build/src/route_runoff.f90
@@ -28,11 +28,11 @@ USE mpi                                          ! MPI
 USE model_setup,         only : init_model       ! model setupt - reading control file, populate metadata, read parameter file
 USE model_setup,         only : init_data        ! initialize river segment data
 
-! subroutines: routing per proc
-USE mpi_routine,         only : comm_runoff_data ! distribute runoff to proc, route them, and gather,
+! subroutines: routing
+USE mpi_routine,         only : comm_runoff_data ! Distribute runoff to proc, route them, and gather,
 
 ! subroutines: model I/O
-USE get_runoff        ,  only : get_hru_runoff
+USE get_runoff        ,  only : get_hru_runoff   !
 USE write_simoutput,     only : prep_output      !
 USE write_simoutput,     only : output           !
 USE write_restart,       only : write_state_nc   ! write netcdf state output file
@@ -42,6 +42,8 @@ USE write_restart,       only : write_state_nc   ! write netcdf state output fil
 ! ************************
 implicit none
 
+character(len=strLen)         :: cfile_name          ! name of the control file
+
 ! error control
 integer(i4b)                  :: ierr                ! error code
 character(len=strLen)         :: cmessage            ! error message of downwind routine
@@ -49,20 +51,23 @@ character(len=strLen)         :: cmessage            ! error message of downwind
 ! index of looping variables
 integer(i4b)                  :: iTime               ! index for time
 
-! ancillary data on model input
+! ancillary data on model input (Should be hidden)
 integer(i4b),allocatable      :: ixSubHRU(:)         ! global HRU index in the order of domains
 integer(i4b),allocatable      :: ixSubSEG(:)         ! global reach index in the order of domains
 integer(i4b),allocatable      :: hru_per_proc(:)     ! number of hru assigned to each proc (i.e., node)
 integer(i4b),allocatable      :: seg_per_proc(:)     ! number of reaches assigned to each proc (i.e., node)
 
-! time variables
-real(dp)                      :: modJulday           ! julian day: model simulation
+logical(lgt)                  :: skip_timestep       ! check if current time step is within start and end of simulation time
 
 ! MPI variables
 integer(i4b)                  :: pid                 ! process id
 integer(i4b)                  :: nNodes              ! number of nodes
 ! ======================================================================================================
 ! ======================================================================================================
+
+! get command-line argument defining the full path to the control file
+ call getarg(1,cfile_name)
+ if(len_trim(cfile_name)==0) call handle_err(50,'need to supply name of the control file as a command-line argument')
 
 ! *****
 ! *** MPI initialization ....
@@ -76,12 +81,12 @@ call MPI_COMM_RANK(MPI_COMM_WORLD, pid, ierr)
 ! *****
 ! *** model setup
 ! ************************
-call init_model(pid, ierr, cmessage)
+call init_model(pid, cfile_name, ierr, cmessage)
 if(ierr/=0) call handle_err(ierr, cmessage)
 
 ! *****
 ! *** data initialization
-!     - river topology, properties (temporarily static)
+!     - river topology, properties
 !     - runoff data (datetime, domain)
 !     - runoff remapping data
 !     - channel states
@@ -103,46 +108,43 @@ if(ierr/=0) call handle_err(ierr, cmessage)
 !endif
 
 ! ***********************************
-! start of time-stepping simulation code
+! start of time-stepping simulation
 ! ***********************************
-do iTime=1,runoff_data%nTime
+!do iTime=1,runoff_data%nTime
+do iTime=1,300
 
-  if (pid==0) then
+  ! prepare simulation output netCDF
+  call prep_output(pid,            & ! input:  proc id
+                   iTime,          & ! input:  time loop index
+                   skip_timestep,  & ! output: current simulation julian day
+                   ierr, cmessage)   ! output: error control
+  if(ierr/=0) call handle_err(ierr, cmessage)
 
-    ! prepare simulation output netCDF
-    call prep_output(iTime,          & ! input:  time loop index
-                     modJulday,      & ! output: current simulation julian day
-                     ierr, cmessage)   ! output: error control
-    if(ierr/=0) call handle_err(ierr, cmessage)
+  ! check we are within the desired time interval
+  if(skip_timestep) cycle
 
-    ! check we are within the desired time interval
-    if(modJulday < startJulday .or. modJulday > endJulday) cycle
+!  if (pid==0) then
 
-    call get_hru_runoff(iTime, ierr, cmessage)
-    if(ierr/=0) call handle_err(ierr, cmessage)
+!    call get_hru_runoff(iTime, ierr, cmessage)
+!    if(ierr/=0) call handle_err(ierr, cmessage)
 
-  end if
+!  end if
  ! print*, 'PAUSE: after getting simulated runoff'; read(*,*)
-
-  ! process routing at each proc
-  call comm_runoff_data(pid,           &  ! input: proc id
-                        nNodes,        &  ! input: number of procs
-                        ixSubHRU,      &  ! input: global HRU index in the order of domains
-                        hru_per_proc,  &  ! input: number of hrus assigned to each proc
-                        seg_per_proc,  &  ! input: number of hrus assigned to each proc
-                        ierr, cmessage)   ! output: error control
-  if(ierr/=0) call handle_err(ierr, cmessage)
-
-  call output(ierr, cmessage)
-  if(ierr/=0) call handle_err(ierr, cmessage)
-
-  ! increment time bounds
-  TSEC(0) = TSEC(0) + dt
-  TSEC(1) = TSEC(0) + dt
+  print*, pid, iTime
+!  ! process routing at each proc
+!  call comm_runoff_data(pid,           &  ! input: proc id
+!                        nNodes,        &  ! input: number of procs
+!                        ixSubHRU,      &  ! input: global HRU index in the order of domains
+!                        hru_per_proc,  &  ! input: number of hrus assigned to each proc
+!                        seg_per_proc,  &  ! input: number of hrus assigned to each proc
+!                        ierr, cmessage)   ! output: error control
+!  if(ierr/=0) call handle_err(ierr, cmessage)
+!
+!  call output(pid, ierr, cmessage)
+!  if(ierr/=0) call handle_err(ierr, cmessage)
 
 end do  ! looping through time
 
-!! Write states to netCDF
 !call write_state_nc(trim(output_dir)//trim(fname_state_out), routOpt, runoff_data%time, 1, TSEC(0), TSEC(1), reachID, ierr, cmessage)
 !if(ierr/=0) call handle_err(ierr, cmessage)
 
