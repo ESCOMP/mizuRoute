34cd71c035b1b20485b18c3d31601d409e902218
Naoki Mizukami <mizukami@ucar.edu>
2019-03-09 14:08:14 -0700

    added saved variables

--- route/build/src/globalData.f90
+++ route/build/src/globalData.f90
@@ -51,6 +51,11 @@ module globalData
 
   save
 
+  ! ---------- MPI variables -------------------------------------------------------------------
+
+  integer(i4b)                  :: pid                 ! process id
+  integer(i4b)                  :: nNodes              ! number of nodes
+
   ! ---------- physical constants -------------------------------------------------------------------
 
   real(dp)          , parameter  , public :: pi=3.14159265359_dp        ! pi
