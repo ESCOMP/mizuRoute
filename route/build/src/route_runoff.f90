3a6f39ac83f88e56036ab4ed15aadd0fe83ef547
Naoki Mizukami <mizukami@ucar.edu>
2019-04-04 07:53:26 -0600

    added timing (maybe temporary)

--- route/build/src/route_runoff.f90
+++ route/build/src/route_runoff.f90
@@ -40,8 +40,15 @@ integer(i4b)                  :: ierr                ! error code
 character(len=strLen)         :: cmessage            ! error message of downwind routine
 integer(i4b)                  :: iens = 1
 logical(lgt)                  :: finished=.false.
+!Timing
+
+integer(i4b)  :: cr, startTime, endTime
+real(dp)      :: rate, elapsedTime
 ! ======================================================================================================
 ! ======================================================================================================
+! Initialize the system_clock
+CALL system_clock(count_rate=cr)
+rate = real(cr)
 
 ! get command-line argument defining the full path to the control file
  call getarg(1,cfile_name)
@@ -76,7 +83,6 @@ if(ierr/=0) call handle_err(ierr, cmessage)
 
 !call MPI_FINALIZE(ierr)
 !stop
-
 ! ***********************************
 ! start of time-stepping simulation
 ! ***********************************
@@ -90,17 +96,33 @@ do while (.not.finished)
 
   ! Get river network hru runoff at current time step
   if(pid==0)then
+call system_clock(startTime)
     call get_hru_runoff(ierr, cmessage)
     if(ierr/=0) call handle_err(ierr, cmessage)
+call system_clock(endTime)
+elapsedTime = real(endTime-startTime, kind(dp))/rate
+write(*,"(A,1PG15.7,A)") '   elapsed-time [read_ro] = ', elapsedTime, ' s'
   endif
 
   ! process routing at each proc
+if(pid==0)then
+call system_clock(startTime)
+endif
   call mpi_route(pid, nNodes, iens, ierr, cmessage)
   if(ierr/=0) call handle_err(ierr, cmessage)
+if(pid==0)then
+call system_clock(endTime)
+elapsedTime = real(endTime-startTime, kind(dp))/rate
+write(*,"(A,1PG15.7,A)") '   elapsed-time [routing] = ', elapsedTime, ' s'
+endif
 
   if(pid==0)then
+call system_clock(startTime)
     call output(ierr, cmessage)
     if(ierr/=0) call handle_err(ierr, cmessage)
+call system_clock(endTime)
+elapsedTime = real(endTime-startTime, kind(dp))/rate
+write(*,"(A,1PG15.7,A)") '   elapsed-time [output] = ', elapsedTime, ' s'
   endif
 
   call update_time(finished, ierr, cmessage)
