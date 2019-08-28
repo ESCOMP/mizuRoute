fc77dfc67db955fdce9e1e0eb91f2c4e12888344
Naoki Mizukami <mizukami@ucar.edu>
2019-08-28 11:12:17 -0600

    change integer type to long for basin ID - take 2

--- route/build/src/nr_utility.f90
+++ route/build/src/nr_utility.f90
@@ -3,8 +3,19 @@ USE nrtype
 ! contains functions that should really be part of the fortran standard, but are not
 implicit none
 INTERFACE arth
- MODULE PROCEDURE arth_r, arth_d, arth_i
+ MODULE PROCEDURE arth_r, arth_d, arth_i4b, arth_i8b
 END INTERFACE
+
+interface indexx
+module procedure indexx_i4b
+module procedure indexx_i8b
+end interface
+
+interface swap
+module procedure swap_i4b
+module procedure swap_i8b
+end interface
+
 ! (everything private unless otherwise specifed)
 private
 public::arth
@@ -44,23 +55,36 @@ contains
  end if
  END FUNCTION arth_d
  ! ------------------------------------------------------------------------------------------------
- FUNCTION arth_i(first,increment,n)
+ FUNCTION arth_i4b(first,increment,n)
  implicit none
  INTEGER(I4B), INTENT(IN) :: first,increment,n
- INTEGER(I4B), DIMENSION(n) :: arth_i
+ INTEGER(I4B), DIMENSION(n) :: arth_i4b
  INTEGER(I4B) :: k
- arth_i(1)=first
+ arth_i4b(1)=first
  if(n>1)then
   do k=2,n
-   arth_i(k) = arth_i(k-1) + increment
+   arth_i4b(k) = arth_i4b(k-1) + increment
   end do
  end if
- END FUNCTION arth_i
+ END FUNCTION arth_i4b
+ ! ------------------------------------------------------------------------------------------------
+ FUNCTION arth_i8b(first,increment,n)
+ implicit none
+ INTEGER(I8B), INTENT(IN) :: first,increment,n
+ INTEGER(I8B), DIMENSION(n) :: arth_i8b
+ INTEGER(I8B) :: k
+ arth_i8b(1)=first
+ if(n>1)then
+  do k=2,n
+   arth_i8b(k) = arth_i8b(k-1) + increment
+  end do
+ end if
+ END FUNCTION arth_i8b
 
  ! *************************************************************************************************
  ! * sort function, used to sort numbers in ascending order
  ! *************************************************************************************************
- SUBROUTINE indexx(arr,index)
+ SUBROUTINE indexx_i4b(arr,index)
  IMPLICIT NONE
  INTEGER(I4B), DIMENSION(:), INTENT(IN) :: arr
  INTEGER(I4B), DIMENSION(:), INTENT(OUT) :: index
@@ -135,16 +159,103 @@ contains
      j=swp
  end if
  END SUBROUTINE icomp_xchg
- END SUBROUTINE indexx
+ END SUBROUTINE indexx_i4b
+ ! ------------------------------------------------------------------------------------------------
+ SUBROUTINE indexx_i8b(arr,index)
+ IMPLICIT NONE
+ INTEGER(I8B), DIMENSION(:), INTENT(IN) :: arr
+ INTEGER(I4B), DIMENSION(:), INTENT(OUT) :: index
+ INTEGER(I4B), PARAMETER :: NN=15, NSTACK=50
+ INTEGER(I8B) :: a
+ INTEGER(I4B) :: n,k,i,j,indext,jstack,l,r
+ INTEGER(I4B), DIMENSION(NSTACK) :: istack
+ n=size(arr)
+ index=arth(1,1,n)
+ jstack=0
+ l=1
+ r=n
+ do
+     if (r-l < NN) then
+         do j=l+1,r
+             indext=index(j)
+             a=arr(indext)
+             do i=j-1,1,-1
+                 if (arr(index(i)) <= a) exit
+                 index(i+1)=index(i)
+             end do
+             index(i+1)=indext
+         end do
+         if (jstack == 0) RETURN
+         r=istack(jstack)
+         l=istack(jstack-1)
+         jstack=jstack-2
+     else
+         k=(l+r)/2
+         call swap(index(k),index(l+1))
+         call icomp_xchg(index(l),index(r))
+         call icomp_xchg(index(l+1),index(r))
+         call icomp_xchg(index(l),index(l+1))
+         i=l+1
+         j=r
+         indext=index(l+1)
+         a=arr(indext)
+         do
+             do
+                 i=i+1
+                 if (arr(index(i)) >= a) exit
+             end do
+             do
+                 j=j-1
+                 if (arr(index(j)) <= a) exit
+             end do
+             if (j < i) exit
+             call swap(index(i),index(j))
+         end do
+         index(l+1)=index(j)
+         index(j)=indext
+         jstack=jstack+2
+         if (r-i+1 >= j-l) then
+             istack(jstack)=r
+             istack(jstack-1)=i
+             r=j-1
+         else
+             istack(jstack)=j-1
+             istack(jstack-1)=l
+             l=i
+         end if
+     end if
+ end do
+ CONTAINS
+ ! internal subroutine
+ SUBROUTINE icomp_xchg(i,j)
+ INTEGER(I4B), INTENT(INOUT) :: i,j
+ INTEGER(I4B) :: swp
+ if (arr(j) < arr(i)) then
+     swp=i
+     i=j
+     j=swp
+ end if
+ END SUBROUTINE icomp_xchg
+ END SUBROUTINE indexx_i8b
 
+ ! ************************************************************************************************
  ! private subroutine
- SUBROUTINE swap(a,b)
+ ! ************************************************************************************************
+ SUBROUTINE swap_i4b(a,b)
  INTEGER(I4B), INTENT(INOUT) :: a,b
  INTEGER(I4B) :: dum
  dum=a
  a=b
  b=dum
- END SUBROUTINE swap
+ END SUBROUTINE swap_i4b
+ ! ------------------------------------------------------------------------------------------------
+ SUBROUTINE swap_i8b(a,b)
+ INTEGER(I8B), INTENT(INOUT) :: a,b
+ INTEGER(I8B) :: dum
+ dum=a
+ a=b
+ b=dum
+ END SUBROUTINE swap_i8b
 
  ! ************************************************************************************************
  ! * findIndex: find the first index within a vector
