a66f9cf0ef34e4190313f644668bedf310965805
ShervanGharari <shg096@gra-login1.graham.sharcnet>
2020-12-08 21:52:56 -0500

    minor change

--- route/build/src/standalone/read_runoff.f90
+++ route/build/src/standalone/read_runoff.f90
@@ -409,7 +409,7 @@ contains
  where ( abs(dummy - input_fillvalue) < verySmall ) dummy = realMissing
 
  ! replace the negative values with zero
- where ( dummy < verySmall ) dummy = 0
+ where ( dummy < verySmall ) dummy = 0._dp
 
  ! reshape
  sim2d(1:nSpace(2),1:nSpace(1)) = dummy(1:nSpace(2),1:nSpace(1),1)
