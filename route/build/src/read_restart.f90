2544cf8bd0010e67158c829b0b6d8ce72a29b335
Naoki Mizukami <mizukami@ucar.edu>
2021-08-27 12:11:27 -0600

    error message changes in restart reading routine

--- route/build/src/read_restart.f90
+++ route/build/src/read_restart.f90
@@ -115,6 +115,7 @@ CONTAINS
   integer(i4b), intent(out)     :: ierr           ! error code
   character(*), intent(out)     :: message1       ! error message
   ! local variables
+  character(len=strLen)         :: cmessage1      ! error message of downwind routine
   type(states)                  :: state          ! temporal state data structures
   integer(i4b)                  :: iVar,iens,iSeg ! loop indices for variables, ensembles, reaches respectively
   integer(i4b)                  :: jSeg           ! sorted index for reaches
@@ -123,23 +124,23 @@ CONTAINS
   ! initialize error control
   ierr=0; message1='read_basinQ_state/'
 
-  allocate(state%var(nVarsBasinQ), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(state%var(nVarsBasinQ), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   do iVar=1,nVarsBasinQ
     select case(iVar)
       case(ixBasinQ%q); allocate(state%var(iVar)%array_2d_dp(nSeg, nens),       stat=ierr)
       case default; ierr=20; message1=trim(message1)//'unable to identify basin routing variable index'; return
     end select
-    if(ierr/=0)then; message1=trim(message1)//'problem allocating space for reach inflow '//trim(meta_basinQ(iVar)%varName); return; endif
+    if(ierr/=0)then; message1=trim(message1)//'problem allocating space for reach inflow:'//trim(meta_basinQ(iVar)%varName); return; endif
   end do
 
   do iVar=1,nVarsBasinQ
     select case(iVar)
-      case(ixBasinQ%q); call get_nc(fname, meta_basinQ(iVar)%varName, state%var(iVar)%array_2d_dp, (/1,1/), (/nSeg,nens/), ierr, cmessage)
+      case(ixBasinQ%q); call get_nc(fname, meta_basinQ(iVar)%varName, state%var(iVar)%array_2d_dp, (/1,1/), (/nSeg,nens/), ierr, cmessage1)
       case default; ierr=20; message1=trim(message1)//'unable to identify previous time step reach inflow variable index for nc writing'; return
     end select
-    if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+    if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':'//trim(meta_basinQ(iVar)%varName); return; endif
   enddo
 
   do iens=1,nens
@@ -166,6 +167,7 @@ CONTAINS
   integer(i4b), intent(out)     :: ierr           ! error code
   character(*), intent(out)     :: message1       ! error message
   ! local variables
+  character(len=strLen)         :: cmessage1      ! error message of downwind routine
   type(states)                  :: state          ! temporal state data structures
   integer(i4b)                  :: iVar,iens,iSeg ! index loops for variables, ensembles, reaches respectively
   integer(i4b)                  :: jSeg           ! index loops for reaches respectively
@@ -173,11 +175,11 @@ CONTAINS
 
   ierr=0; message1='read_IRFbas_state/'
 
-  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%tdh)%dimName), ntdh, ierr, cmessage)
-  if(ierr/=0)then;  message1=trim(message1)//trim(cmessage); return; endif
+  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%tdh)%dimName), ntdh, ierr, cmessage1)
+  if(ierr/=0)then;  message1=trim(message1)//trim(cmessage1); return; endif
 
-  allocate(state%var(nVarsIRFbas), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(state%var(nVarsIRFbas), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   do iVar=1,nVarsIRFbas
 
@@ -185,17 +187,17 @@ CONTAINS
     case(ixIRFbas%qfuture); allocate(state%var(iVar)%array_3d_dp(nSeg, ntdh, nens), stat=ierr)
     case default; ierr=20; message1=trim(message1)//'unable to identify basin routing variable index'; return
    end select
-   if(ierr/=0)then; message1=trim(message1)//'problem allocating space for basin IRF routing state '//trim(meta_irf_bas(iVar)%varName); return; endif
+   if(ierr/=0)then; message1=trim(message1)//'problem allocating space for basin IRF routing state:'//trim(meta_irf_bas(iVar)%varName); return; endif
 
   end do
 
   do iVar=1,nVarsIRFbas
 
    select case(iVar)
-    case(ixIRFbas%qfuture); call get_nc(fname, meta_irf_bas(iVar)%varName, state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,ntdh,nens/), ierr, cmessage)
+    case(ixIRFbas%qfuture); call get_nc(fname, meta_irf_bas(iVar)%varName, state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,ntdh,nens/), ierr, cmessage1)
     case default; ierr=20; message1=trim(message1)//'unable to identify basin IRF variable index for nc writing'; return
    end select
-   if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+   if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':'//trim(meta_irf_bas(iVar)%varName); return; endif
 
   enddo
 
@@ -204,8 +206,8 @@ CONTAINS
 
     jSeg = ixRch_order(iSeg)
 
-    allocate(RCHFLX(iens,jSeg)%QFUTURE(ntdh), stat=ierr, errmsg=cmessage)
-    if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+    allocate(RCHFLX(iens,jSeg)%QFUTURE(ntdh), stat=ierr, errmsg=cmessage1)
+    if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
     do iVar=1,nVarsIRFbas
 
@@ -230,6 +232,7 @@ CONTAINS
   integer(i4b), intent(out)     :: ierr           ! error code
   character(*), intent(out)     :: message1       ! error message
   ! local variables
+  character(len=strLen)         :: cmessage1      ! error message of downwind routine
   type(states)                  :: state          ! temporal state data structures
   integer(i4b)                  :: iVar,iens,iSeg ! index loops for variables, ensembles, reaches respectively
   integer(i4b)                  :: jSeg           ! index loops for reaches respectively
@@ -239,14 +242,14 @@ CONTAINS
 
   ierr=0; message1='read_IRF_state/'
 
-  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%tdh_irf)%dimName), ntdh_irf, ierr, cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%tdh_irf)%dimName), ntdh_irf, ierr, cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
-  allocate(state%var(nVarsIRF), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(state%var(nVarsIRF), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
-  allocate(numQF(nens,nSeg), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(numQF(nens,nSeg), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   do iVar=1,nVarsIRF
 
@@ -255,21 +258,21 @@ CONTAINS
     case(ixIRF%irfVol);  allocate(state%var(iVar)%array_3d_dp(nSeg, nTbound, nens), stat=ierr)
     case default; ierr=20; message1=trim(message1)//'unable to identify variable index'; return
    end select
-   if(ierr/=0)then; message1=trim(message1)//'problem allocating space for IRF routing state '//trim(meta_irf(iVar)%varName); return; endif
+   if(ierr/=0)then; message1=trim(message1)//'problem allocating space for IRF routing state:'//trim(meta_irf(iVar)%varName); return; endif
 
   end do
 
-  call get_nc(fname,'numQF',numQF,(/1,1/),(/nSeg,nens/),ierr,cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  call get_nc(fname,'numQF',numQF,(/1,1/),(/nSeg,nens/),ierr,cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':numQF'; return; endif
 
   do iVar=1,nVarsIRF
 
    select case(iVar)
-    case(ixIRF%qfuture); call get_nc(fname, meta_irf(iVar)%varName, state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,ntdh_irf,nens/), ierr, cmessage)
-    case(ixIRF%irfVol);  call get_nc(fname, meta_irf(iVar)%varName, state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nTbound, nens/), ierr, cmessage)
+    case(ixIRF%qfuture); call get_nc(fname, meta_irf(iVar)%varName, state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,ntdh_irf,nens/), ierr, cmessage1)
+    case(ixIRF%irfVol);  call get_nc(fname, meta_irf(iVar)%varName, state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nTbound, nens/), ierr, cmessage1)
     case default; ierr=20; message1=trim(message1)//'unable to identify IRF variable index for nc reading'; return
    end select
-   if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+   if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':'//trim(meta_irf(iVar)%varName); return; endif
 
   end do
 
@@ -278,8 +281,8 @@ CONTAINS
 
     jSeg = ixRch_order(iSeg)
 
-    allocate(RCHFLX(iens,jSeg)%QFUTURE_IRF(numQF(iens,iSeg)), stat=ierr, errmsg=cmessage)
-    if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+    allocate(RCHFLX(iens,jSeg)%QFUTURE_IRF(numQF(iens,iSeg)), stat=ierr, errmsg=cmessage1)
+    if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
     do iVar=1,nVarsIRF
 
@@ -305,6 +308,7 @@ CONTAINS
   integer(i4b), intent(out)     :: ierr           ! error code
   character(*), intent(out)     :: message1       ! error message
   ! local variables
+  character(len=strLen)         :: cmessage1      ! error message of downwind routine
   type(states)                  :: state          ! temporal state data structures
   integer(i4b)                  :: iVar,iens,iSeg ! index loops for variables, ensembles, reaches respectively
   integer(i4b)                  :: jSeg           ! index loops for reaches respectively
@@ -314,15 +318,15 @@ CONTAINS
 
   ierr=0; message1='read_KWT_state/'
 
-  allocate(state%var(nVarsKWT), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(state%var(nVarsKWT), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
-  allocate(numWaves(nens,nSeg), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(numWaves(nens,nSeg), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   ! get Dimension sizes
-  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%wave)%dimName), nwave, ierr, cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%wave)%dimName), nwave, ierr, cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   do iVar=1,nVarsKWT
 
@@ -332,22 +336,22 @@ CONTAINS
       allocate(state%var(iVar)%array_3d_dp(nSeg, nwave, nens), stat=ierr)
      case default; ierr=20; message1=trim(message1)//'unable to identify variable index'; return
     end select
-    if(ierr/=0)then; message1=trim(message1)//'problem allocating space for KWT routing state '//trim(meta_kwt(iVar)%varName); return; endif
+    if(ierr/=0)then; message1=trim(message1)//'problem allocating space for KWT routing state:'//trim(meta_kwt(iVar)%varName); return; endif
   end do
 
-  call get_nc(fname,'numWaves',numWaves, (/1,1/), (/nSeg,nens/), ierr, cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  call get_nc(fname,'numWaves',numWaves, (/1,1/), (/nSeg,nens/), ierr, cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':'//'numWaves'; return; endif
 
   do iVar=1,nVarsKWT
 
     select case(iVar)
      case(ixKWT%routed)
-      call get_nc(fname,trim(meta_kwt(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nwave,nens/), ierr, cmessage)
+      call get_nc(fname,trim(meta_kwt(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nwave,nens/), ierr, cmessage1)
      case(ixKWT%tentry, ixKWT%texit, ixKWT%qwave, ixKWT%qwave_mod)
-      call get_nc(fname,trim(meta_kwt(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nwave,nens/), ierr, cmessage)
-     case default; ierr=20; message1=trim(message)//'unable to identify KWT variable index for nc reading'; return
+      call get_nc(fname,trim(meta_kwt(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nwave,nens/), ierr, cmessage1)
+     case default; ierr=20; message1=trim(message1)//'unable to identify KWT variable index for nc reading'; return
     end select
-   if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+   if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':'//trim(meta_kwt(iVar)%varName); return; endif
   end do
 
   do iens=1,nens
@@ -388,6 +392,7 @@ CONTAINS
   integer(i4b), intent(out)     :: ierr           ! error code
   character(*), intent(out)     :: message1       ! error message
   ! local variables
+  character(len=strLen)         :: cmessage1      ! error message of downwind routine
   type(states)                  :: state          ! temporal state data structures
   integer(i4b)                  :: iVar,iens,iSeg ! index loops for variables, ensembles, reaches respectively
   integer(i4b)                  :: jSeg           ! index loops for reaches respectively
@@ -395,12 +400,12 @@ CONTAINS
 
   ierr=0; message1='read_KWE_state/'
 
-  allocate(state%var(nVarsKWE), stat=ierr, errmsg=cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  allocate(state%var(nVarsKWE), stat=ierr, errmsg=cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   ! get Dimension sizes
-  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%fdmesh)%dimName), nMesh, ierr, cmessage)
-  if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+  call get_nc_dim_len(fname, trim(meta_stateDims(ixStateDims%fdmesh)%dimName), nMesh, ierr, cmessage1)
+  if(ierr/=0)then; message1=trim(message1)//trim(cmessage1); return; endif
 
   do iVar=1,nVarsKWE
 
@@ -409,19 +414,19 @@ CONTAINS
      case(ixKWE%q); allocate(state%var(iVar)%array_3d_dp(nSeg, nMesh, nens), stat=ierr)
      case default; ierr=20; message1=trim(message1)//'unable to identify variable index'; return
     end select
-    if(ierr/=0)then; message1=trim(message1)//'problem allocating space for KWE routing state '//trim(meta_kwe(iVar)%varName); return; endif
+    if(ierr/=0)then; message1=trim(message1)//'problem allocating space for KWE routing state:'//trim(meta_kwe(iVar)%varName); return; endif
   end do
 
   do iVar=1,nVarsKWE
 
     select case(iVar)
      case(ixKWE%a)
-      call get_nc(fname,trim(meta_kwe(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nMesh,nens/), ierr, cmessage)
+      call get_nc(fname,trim(meta_kwe(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nMesh,nens/), ierr, cmessage1)
      case(ixKWE%q)
-      call get_nc(fname,trim(meta_kwe(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nMesh,nens/), ierr, cmessage)
-     case default; ierr=20; message1=trim(message)//'unable to identify KWE variable index for nc reading'; return
+      call get_nc(fname,trim(meta_kwe(iVar)%varName), state%var(iVar)%array_3d_dp, (/1,1,1/), (/nSeg,nMesh,nens/), ierr, cmessage1)
+     case default; ierr=20; message1=trim(message1)//'unable to identify KWE variable index for nc reading'; return
     end select
-   if(ierr/=0)then; message1=trim(message1)//trim(cmessage); return; endif
+   if(ierr/=0)then; message1=trim(message1)//trim(cmessage1)//':'//trim(meta_kwe(iVar)%varName); return; endif
   end do
 
   do iens=1,nens
