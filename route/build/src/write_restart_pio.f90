4188f6ee396384d21d6336736bcf2cd14c65a708
Naoki Mizukami <mizukami@ucar.edu>
2023-04-25 15:47:32 -0600

    Bugfix on writing history variables in restart.
    The reach flux array in the main core includes mainstem elements, halo elements, and tributary elements in the order.
    The halo elements should not be written so those elements need to be skipped.

--- route/build/src/write_restart_pio.f90
+++ route/build/src/write_restart_pio.f90
@@ -1410,11 +1410,27 @@ CONTAINS
     integer(i4b), intent(out)  :: ierr            ! error code
     character(*), intent(out)  :: message1        ! error message
     ! local variables
+    integer(i4b)               :: nRch_local         ! number of reaches per processors
+    integer(i4b), allocatable  :: index_write(:)  ! indices in hVar to be written in netcdf
     real(sp),     allocatable  :: array_sp(:)
 
     ierr=0; message1='write_history_state/'
 
-    allocate(array_sp(hVars%nRch),stat=ierr, errmsg=cmessage)
+    ! compute index array for each processors (herer
+    if (masterproc) then
+      nRch_local = sum(rch_per_proc(-1:pid))
+      allocate(index_write(nRch_local))
+      if (nRch_mainstem>0) then
+        index_write(1:nRch_mainstem) = arth(1,1,nRch_mainstem)
+      end if
+      index_write(nRch_mainstem+1:nRch_local) = arth(nRch_mainstem+nTribOutlet+1, 1, rch_per_proc(0))
+    else
+      nRch_local = rch_per_proc(pid)
+      allocate(index_write(nRch_local))
+      index_write = arth(1,1,nRch_local)
+    end if
+
+    allocate(array_sp(nRch_local),stat=ierr, errmsg=cmessage)
 
     call write_scalar_netcdf(pioFileDescState, 'nt', hVars%nt, ierr, cmessage)
     if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
@@ -1425,53 +1441,55 @@ CONTAINS
     end if
 
     if (meta_rflx(ixRFLX%instRunoff)%varFile) then
-      call write_pnetcdf(pioFileDescState, 'instRunoff', hVars%instRunoff, ioDesc_rch_float, ierr, cmessage)
+      array_sp(1:nRch_local) = hVars%instRunoff(index_write)
+      call write_pnetcdf(pioFileDescState, 'instRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%dlayRunoff)%varFile) then
-      call write_pnetcdf(pioFileDescState, 'dlayRunoff', hVars%dlayRunoff, ioDesc_rch_float, ierr, cmessage)
+      array_sp(1:nRch_local) = hVars%dlayRunoff(index_write)
+      call write_pnetcdf(pioFileDescState, 'dlayRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%sumUpstreamRunoff)%varFile) then
-      array_sp = hVars%discharge(:, idxSUM)
+      array_sp = hVars%discharge(index_write, idxSUM)
       call write_pnetcdf(pioFileDescState, 'sumUpstreamRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%KWTroutedRunoff)%varFile) then
-      array_sp = hVars%discharge(:, idxKWT)
+      array_sp = hVars%discharge(index_write, idxKWT)
       call write_pnetcdf(pioFileDescState, 'KWTroutedRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%IRFroutedRunoff)%varFile) then
-      array_sp = hVars%discharge(:, idxIRF)
+      array_sp = hVars%discharge(index_write, idxIRF)
       call write_pnetcdf(pioFileDescState, 'IRFroutedRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%KWroutedRunoff)%varFile) then
-      array_sp = hVars%discharge(:, idxKW)
+      array_sp = hVars%discharge(index_write, idxKW)
       call write_pnetcdf(pioFileDescState, 'KWroutedRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%MCroutedRunoff)%varFile) then
-      array_sp = hVars%discharge(:, idxMC)
+      array_sp = hVars%discharge(index_write, idxMC)
       call write_pnetcdf(pioFileDescState, 'MCroutedRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%DWroutedRunoff)%varFile) then
-      array_sp = hVars%discharge(:, idxDW)
+      array_sp = hVars%discharge(index_write, idxDW)
       call write_pnetcdf(pioFileDescState, 'DWroutedRunoff', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
 
     if (meta_rflx(ixRFLX%IRFvolume)%varFile) then
-      array_sp = hVars%volume(:, idxIRF)
+      array_sp = hVars%volume(index_write, idxIRF)
       call write_pnetcdf(pioFileDescState, 'IRFvolume', array_sp, ioDesc_rch_float, ierr, cmessage)
       if(ierr/=0)then; message=trim(message)//trim(cmessage); return; endif
     endif
