e5fd28741983be319ba5f83f384215855327cc58
Erik Kluzek <erik@ucar.edu>
2023-05-27 15:04:39 -0600

    Go back to having this as a full error

--- route/build/src/process_remap.f90
+++ route/build/src/process_remap.f90
@@ -394,12 +394,9 @@ MODULE process_remap_module
      ! error check - runoff depth cannot be negative (no missing value)
      if (limit) then
        if( basinRunoff( hruContribIx(iHRU) ) < negRunoffTol )then
-        ! For now just report on where the negative runoff is coming from
         write(iulog,*) 'Exceeded negative runoff tolerance: HRU = ', hruContribId(iHRU), ' runoff = ', basinRunoff( hruContribIx(iHRU) )
-        ! Later we'll abort when this is detected...
-        !write(message,'(a,i12, g12.2)') trim(message)//'exceeded negative runoff tolerance for HRU ', hruContribId(iHRU), &
-               !basinRunoff( hruContribIx(iHRU) )
-        !ierr=20; return
+        write(message,'(a,i12, g12.2)') trim(message)//'exceeded negative runoff tolerance for HRU ', hruContribId(iHRU)
+        ierr=20; return
        endif
      end if
 
