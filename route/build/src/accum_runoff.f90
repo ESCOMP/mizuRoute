580305386fb2204ea8c40f32671914e1ba140c97
Naoki Mizukami <mizukami@ucar.edu>
2023-06-26 06:13:57 -0600

    define specific each routing method

--- route/build/src/accum_runoff.f90
+++ route/build/src/accum_runoff.f90
@@ -1,147 +1,64 @@
-MODULE accum_runoff_module
+MODULE accum_route_module
 
 ! Accumulate upstream flow instantaneously
+! This is not used as routed runoff.
+! This is used to get total instantaneous upstream runoff at each reach
 
 USE nrtype
-! data type
-USE dataTypes,   ONLY: STRFLX         ! fluxes in each reach
-USE dataTypes,   ONLY: RCHTOPO        ! Network topology
-USE dataTypes,   ONLY: subbasin_omp   ! mainstem+tributary data structures
-! global data
-USE public_var,  ONLY: iulog         ! i/o logical unit number
-USE globalData,  ONLY: idxSUM        ! index of accumulation method
-! subroutines: general
-USE perf_mod,    ONLY: t_startf,t_stopf ! timing start/stop
-USE model_utils, ONLY: handle_err
+USE dataTypes,   ONLY: STRFLX          ! fluxes in each reach
+USE dataTypes,   ONLY: STRSTA          ! state in each reach
+USE dataTypes,   ONLY: RCHTOPO         ! Network topology
+USE dataTypes,   ONLY: RCHPRP          ! Reach parameter
+USE public_var,  ONLY: iulog           ! i/o logical unit number
+USE globalData,  ONLY: idxSUM          ! index of accumulation method
+USE base_route,  ONLY: base_route_rch
 
 implicit none
 
 private
-public::accum_runoff
+public::accum_route_rch
+
+type, extends(base_route_rch) :: accum_route_rch
+ CONTAINS
+   procedure, pass :: route => accum_inst_runoff
+end type accum_route_rch
 
 CONTAINS
-
- ! ---------------------------------------------------------------------------------------
- ! Public subroutine main driver for basin routing
- ! ---------------------------------------------------------------------------------------
- SUBROUTINE accum_runoff(iEns,          & ! input: index of runoff ensemble to be processed
-                         river_basin,   & ! input: river basin information (mainstem, tributary outlet etc.)
-                         ixDesire,      & ! input: ReachID to be checked by on-screen printing
-                         NETOPO_in,     & ! input: reach topology data structure
-                         RCHFLX_out,    & ! inout: reach flux data structure
-                         ierr, message, & ! output: error controls
-                         ixSubRch)        ! optional input: subset of reach indices to be processed
- ! ----------------------------------------------------------------------------------------
- ! Purpose:
- !
- ! Accumulate all the upstream delayed runoff for each reach
- ! This is not used as routed runoff.
- ! This is used to get total instantaneous upstream runoff at each reach
- !
- ! ----------------------------------------------------------------------------------------
-
- implicit none
- ! argument variables
- integer(i4b),                    intent(in)    :: iens            ! runoff ensemble index
- type(subbasin_omp), allocatable, intent(in)    :: river_basin(:)  ! river basin information (mainstem, tributary outlet etc.)
- integer(i4b),                    intent(in)    :: ixDesire        ! index of the reach for verbose output
- type(RCHTOPO),      allocatable, intent(in)    :: NETOPO_in(:)    ! River Network topology
- type(STRFLX),                    intent(inout) :: RCHFLX_out(:,:) ! Reach fluxes (ensembles, space [reaches]) for decomposed domains
- integer(i4b),                    intent(out)   :: ierr            ! error code
- character(*),                    intent(out)   :: message         ! error message
- integer(i4b),       optional,    intent(in)    :: ixSubRch(:)     ! subset of reach indices to be processed
- ! local variables
- integer(i4b)                                   :: nSeg            ! number of segments in the network
- integer(i4b)                                   :: nTrib           ! number of tributaries
- integer(i4b)                                   :: nDom            ! number of domains defined by e.g., stream order, tributary/mainstem
- integer(i4b)                                   :: iSeg, jSeg      ! reach segment indices
- integer(i4b)                                   :: iTrib, ix       ! loop indices
- logical(lgt), allocatable                      :: doRoute(:)      ! logical to indicate which reaches are processed
- character(len=strLen)                          :: cmessage        ! error message from subroutines
-
- ierr=0; message='accum_runoff/'
-
- if (size(NETOPO_in)/=size(RCHFLX_out(iens,:))) then
-  ierr=20; message=trim(message)//'sizes of NETOPO and RCHFLX mismatch'; return
- endif
-
- nSeg = size(RCHFLX_out(iens,:))
-
- allocate(doRoute(nSeg), stat=ierr)
- if(ierr/=0)then; message=trim(message)//'unable to allocate space for [doRoute]'; return; endif
-
- ! if a subset of reaches is processed
- if (present(ixSubRch))then
-   doRoute(:)=.false.
-   doRoute(ixSubRch) = .true. ! only subset of reaches are on
- ! if all the reaches are processed
- else
-   doRoute(:)=.true. ! every reach is on
- endif
-
- nDom = size(river_basin)
-
- call t_startf('route/accum-runoff')
-
- do ix = 1,nDom
-   ! 1. Route tributary reaches (parallel)
-   ! compute the sum of all upstream runoff at each point in the river network
-   nTrib=size(river_basin(ix)%branch)
-
-!$OMP PARALLEL DO schedule(dynamic,1)         &
-!$OMP          private(jSeg, iSeg)            & ! private for a given thread
-!$OMP          private(ierr, cmessage)        & ! private for a given thread
-!$OMP          shared(river_basin)            & ! data structure shared
-!$OMP          shared(doRoute)                & ! data array shared
-!$OMP          shared(NETOPO_in)              & ! data structure shared
-!$OMP          shared(RCHFLX_out)             & ! data structure shared
-!$OMP          shared(ix, iEns, ixDesire)     & ! indices shared
-!$OMP          firstprivate(nTrib)
-   do iTrib = 1,nTrib
-     do iSeg=1,river_basin(ix)%branch(iTrib)%nRch
-       jSeg = river_basin(ix)%branch(iTrib)%segIndex(iSeg)
-
-       if (.not. doRoute(jSeg)) cycle
-
-       call accum_qupstream(iens, jSeg, ixDesire, NETOPO_in, RCHFLX_out, ierr, cmessage)
-       if(ierr/=0) call handle_err(ierr, trim(message)//trim(cmessage))
-
-     end do
-   end do
-!$OMP END PARALLEL DO
-
- end do
-
- call t_stopf('route/accum-runoff')
-
- END SUBROUTINE accum_runoff
 
  ! *********************************************************************
  ! subroutine: perform accumulate immediate upstream flow
  ! *********************************************************************
- SUBROUTINE accum_qupstream(iEns,       &    ! input: index of runoff ensemble to be processed
-                            segIndex,   &    ! input: index of reach to be processed
-                            ixDesire,   &    ! input: reachID to be checked by on-screen pringing
-                            NETOPO_in,  &    ! input: reach topology data structure
-                            RCHFLX_out, &    ! inout: reach flux data structure
-                            ierr, message)   ! output: error control
+ SUBROUTINE accum_inst_runoff(this,         &
+                              iEns,         &  ! input: index of runoff ensemble to be processed
+                              segIndex,     &  ! input: index of reach to be processed
+                              ixDesire,     &  ! input: reachID to be checked by on-screen pringing
+                              T0,T1,        &  ! input: start and end of the time step
+                              NETOPO_in,    &  ! input: reach topology data structure
+                              RPARAM_in,    &  ! input: reach parameter data structure
+                              RCHSTA_out,   &  ! inout: reach state data structure
+                              RCHFLX_out,   &  ! inout: reach flux data structure
+                              ierr, message)   ! output: error control
  implicit none
- ! argument variables
- integer(i4b), intent(in)                 :: iEns           ! runoff ensemble to be routed
- integer(i4b), intent(in)                 :: segIndex       ! segment where routing is performed
- integer(i4b), intent(in)                 :: ixDesire       ! index of the reach for verbose output
- type(RCHTOPO),intent(in),    allocatable :: NETOPO_in(:)   ! River Network topology
- type(STRFLX), intent(inout)              :: RCHFLX_out(:,:)! Reach fluxes (ensembles, space [reaches]) for decomposed domains
- integer(i4b), intent(out)                :: ierr           ! error code
- character(*), intent(out)                :: message        ! error message
+ ! Argument variables
+ class(accum_route_rch)                   :: this
+ integer(i4b),  intent(in)                :: iEns            ! runoff ensemble to be routed
+ integer(i4b),  intent(in)                :: segIndex        ! segment where routing is performed
+ integer(i4b),  intent(in)                :: ixDesire        ! index of the reach for verbose output
+ real(dp),      intent(in)                :: T0,T1           ! start and end of the time step (seconds)
+ type(RCHTOPO), intent(in),   allocatable :: NETOPO_in(:)    ! River Network topology
+ type(RCHPRP),  intent(inout),allocatable :: RPARAM_in(:)    ! River reach parameter
+ type(STRSTA),  intent(inout)             :: RCHSTA_out(:,:) ! reach state data
+ type(STRFLX),  intent(inout)             :: RCHFLX_out(:,:) ! Reach fluxes (ensembles, space [reaches]) for decomposed domains
+ integer(i4b),  intent(out)               :: ierr            ! error code
+ character(*),  intent(out)               :: message         ! error message
  ! Local variables
- real(dp)                                 :: q_upstream     ! upstream Reach fluxes
- integer(i4b)                             :: nUps           ! number of upstream segment
- integer(i4b)                             :: iUps           ! upstream reach index
- integer(i4b)                             :: iRch_ups       ! index of upstream reach in NETOPO
- character(len=strLen)                    :: fmt1,fmt2      ! format string
+ real(dp)                                 :: q_upstream      ! upstream Reach fluxes
+ integer(i4b)                             :: nUps            ! number of upstream segment
+ integer(i4b)                             :: iUps            ! upstream reach index
+ integer(i4b)                             :: iRch_ups        ! index of upstream reach in NETOPO
+ character(len=strLen)                    :: fmt1,fmt2       ! format string
 
- ierr=0; message='accum_qupstream/'
+ ierr=0; message='accum_inst_runoff/'
 
  ! identify number of upstream segments of the reach being processed
  nUps = size(NETOPO_in(segIndex)%UREACHI)
@@ -176,6 +93,6 @@ CONTAINS
    write(iulog,'(a,x,G15.4)')     ' RCHFLX_out%ROUTE(idxSUM)%REACH_Q =', RCHFLX_out(iens,segIndex)%ROUTE(idxSUM)%REACH_Q
  endif
 
- END SUBROUTINE accum_qupstream
+ END SUBROUTINE accum_inst_runoff
 
-END MODULE accum_runoff_module
+END MODULE accum_route_module
--- route/build/src/irf_route.f90
+++ route/build/src/irf_route.f90
@@ -1,130 +1,42 @@
 MODULE irf_route_module
 
 USE nrtype
-! data type
-USE dataTypes, ONLY: STRFLX           ! fluxes in each reach
-USE dataTypes, ONLY: RCHTOPO          ! Network topology
-USE dataTypes, ONLY: RCHPRP           ! reach/lake property parameter
-USE dataTypes, ONLY: irfRCH           ! irf specific state data structure
-USE dataTypes, ONLY: subbasin_omp     ! mainstem+tributary data structures
-! global parameters
-USE public_var, ONLY: iulog           ! i/o logical unit number
-USE public_var, ONLY: realMissing     ! missing value for real number
-USE public_var, ONLY: integerMissing  ! missing value for integer number
-USE public_var, ONLY: dt
-USE globalData, ONLY: idxIRF          ! index of IRF method
-! external subroutines
-USE lake_route_module, ONLY: lake_route        ! lake route module
+USE dataTypes,         ONLY: STRFLX            ! fluxes in each reach
+USE dataTypes,         ONLY: STRSTA            ! state in each reach
+USE dataTypes,         ONLY: RCHTOPO           ! Network topology
+USE dataTypes,         ONLY: RCHPRP            ! reach/lake property parameter
+USE dataTypes,         ONLY: irfRCH            ! irf specific state data structure
+USE public_var,        ONLY: iulog             ! i/o logical unit number
+USE public_var,        ONLY: realMissing       ! missing value for real number
+USE public_var,        ONLY: integerMissing    ! missing value for integer number
+USE public_var,        ONLY: dt
+USE globalData,        ONLY: idxIRF            ! index of IRF method
 USE water_balance,     ONLY: comp_reach_wb     ! compute water balance error
-USE perf_mod,          ONLY: t_startf,t_stopf  ! timing start/stop
-USE model_utils,       ONLY: handle_err
+USE base_route,        ONLY: base_route_rch
 
 implicit none
 
 private
-public::irf_route
+public::irf_route_rch
+
+type, extends(base_route_rch) :: irf_route_rch
+ CONTAINS
+   procedure, pass :: route => irf_rch
+end type irf_route_rch
 
 CONTAINS
 
  ! *********************************************************************
- ! subroutine: perform network UH routing
- ! *********************************************************************
- SUBROUTINE irf_route(iEns,          &  ! input: index of runoff ensemble to be processed
-                      river_basin,   &  ! input: river basin information (mainstem, tributary outlet etc.)
-                      ixDesire,      &  ! input: reachID to be checked by on-screen pringing
-                      NETOPO_in,     &  ! input: reach topology data structure
-                      RPARAM_in,     &  ! input: reach parameter data structure
-                      RCHFLX_out,    &  ! inout: reach flux data structure
-                      ierr, message, &  ! output: error control
-                      ixSubRch)         ! optional input: subset of reach indices to be processed
-
- USE public_var,  ONLY: is_lake_sim    ! logical whether or not lake should be simulated
-
- implicit none
- ! argument variables
- integer(i4b),       intent(in)                  :: iEns                ! runoff ensemble to be routed
- type(subbasin_omp), intent(in),    allocatable  :: river_basin(:)      ! river basin information (mainstem, tributary outlet etc.)
- integer(i4b),       intent(in)                  :: ixDesire            ! index of the reach for verbose output ! Output
- type(RCHTOPO),      intent(in),    allocatable  :: NETOPO_in(:)        ! River Network topology
- type(RCHPRP),       intent(inout), allocatable  :: RPARAM_in(:)        ! River Network parameters
- TYPE(STRFLX),       intent(inout)               :: RCHFLX_out(:,:)     ! Reach fluxes (ensembles, space [reaches]) for decomposed domains
- integer(i4b),       intent(out)                 :: ierr                ! error code
- character(*),       intent(out)                 :: message             ! error message
- integer(i4b),       intent(in),    optional     :: ixSubRch(:)         ! subset of reach indices to be processed
- ! Local variables
- character(len=strLen)                           :: cmessage            ! error message from subroutine
- logical(lgt),                      allocatable  :: doRoute(:)          ! logical to indicate which reaches are processed
- integer(i4b)                                    :: nOrder              ! number of stream order
- integer(i4b)                                    :: nTrib               ! number of tributary basins
- integer(i4b)                                    :: nSeg                ! number of reaches in the network
- integer(i4b)                                    :: iSeg, jSeg          ! loop indices - reach
- integer(i4b)                                    :: iTrib               ! loop indices - branch
- integer(i4b)                                    :: ix                  ! loop indices stream order
-
- ierr=0; message='irf_route/'
-
- ! number of reach check
- if (size(NETOPO_in)/=size(RCHFLX_out(iens,:))) then
-  ierr=20; message=trim(message)//'sizes of NETOPO and RCHFLX mismatch'; return
- endif
-
- nSeg = size(RCHFLX_out(iens,:))
-
- allocate(doRoute(nSeg), stat=ierr)
-
- if (present(ixSubRch))then
-  doRoute(:)=.false.
-  doRoute(ixSubRch) = .true. ! only subset of reaches are on
- else
-  doRoute(:)=.true. ! every reach is on
- endif
-
- nOrder = size(river_basin)
-
- call t_startf('route/irf')
-
- do ix = 1,nOrder
-
-   nTrib=size(river_basin(ix)%branch)
-
-  ! 1. Route tributary reaches (parallel)
-!$OMP PARALLEL DO schedule(dynamic,1)                   &
-!$OMP          private(jSeg, iSeg)                      & ! private for a given thread
-!$OMP          private(ierr, cmessage)                  & ! private for a given thread
-!$OMP          shared(river_basin)                      & ! data structure shared
-!$OMP          shared(doRoute)                          & ! data array shared
-!$OMP          shared(NETOPO_in)                        & ! data structure shared
-!$OMP          shared(RPARAM_in)                        & ! data structure shared
-!$OMP          shared(RCHFLX_out)                       & ! data structure shared
-!$OMP          shared(ix, iEns, ixDesire)               & ! indices shared
-!$OMP          firstprivate(nTrib)
-   trib:do iTrib = 1,nTrib
-     seg:do iSeg=1,river_basin(ix)%branch(iTrib)%nRch
-       jSeg = river_basin(ix)%branch(iTrib)%segIndex(iSeg)
-       if (.not. doRoute(jSeg)) cycle
-       if ((NETOPO_in(jseg)%islake).and.(is_lake_sim)) then
-        call lake_route (iEns, jSeg, ixDesire, NETOPO_in, RPARAM_in, RCHFLX_out, ierr, cmessage)
-       else
-        call irf_rch(iEns, jSeg, ixDesire, NETOPO_in,            RCHFLX_out, ierr, cmessage)
-       endif
-       if(ierr/=0) call handle_err(ierr, trim(message)//trim(cmessage))
-     end do seg
-   end do trib
-!$OMP END PARALLEL DO
-
- end do
-
- call t_stopf('route/irf')
-
- end subroutine irf_route
-
- ! *********************************************************************
  ! subroutine: perform one segment route UH routing
  ! *********************************************************************
- SUBROUTINE irf_rch(iEns,         & ! input: index of runoff ensemble to be processed
-                    segIndex,     & ! input: index of runoff ensemble to be processed
+ SUBROUTINE irf_rch(this,         & !
+                    iEns,         & ! input: index of runoff ensemble
+                    segIndex,     & ! input: reach index
                     ixDesire,     & ! input: reachID to be checked by on-screen pringing
+                    T0,T1,        & ! input: start and end of the time step
                     NETOPO_in,    & ! input: reach topology data structure
+                    RPARAM_in,    & ! input: reach parameter data structure
+                    RCHSTA_out,   & ! inout: reach state data structure
                     RCHFLX_out,   & ! inout: reach flux data structure
                     ierr, message)  ! output: error control
 
@@ -132,13 +44,17 @@ CONTAINS
 
  implicit none
  ! Argument variables
- integer(i4b), intent(in)                 :: iEns           ! runoff ensemble to be routed
- integer(i4b), intent(in)                 :: segIndex       ! segment where routing is performed
- integer(i4b), intent(in)                 :: ixDesire       ! index of the reach for verbose output
- type(RCHTOPO),intent(in),    allocatable :: NETOPO_in(:)   ! River Network topology
- type(STRFLX), intent(inout)              :: RCHFLX_out(:,:)! Reach fluxes (ensembles, space [reaches]) for decomposed domains
- integer(i4b), intent(out)                :: ierr           ! error code
- character(*), intent(out)                :: message        ! error message
+ class(irf_route_rch)                     :: this
+ integer(i4b),  intent(in)                :: iEns            ! runoff ensemble to be routed
+ integer(i4b),  intent(in)                :: segIndex        ! segment where routing is performed
+ integer(i4b),  intent(in)                :: ixDesire        ! index of the reach for verbose output
+ real(dp),      intent(in)                :: T0,T1           ! start and end of the time step (seconds)
+ type(RCHTOPO), intent(in),   allocatable :: NETOPO_in(:)    ! River Network topology
+ type(RCHPRP),  intent(inout),allocatable :: RPARAM_in(:)    ! River reach parameter
+ type(STRSTA),  intent(inout)             :: RCHSTA_out(:,:) ! reach state data
+ type(STRFLX),  intent(inout)             :: RCHFLX_out(:,:) ! Reach fluxes (ensembles, space [reaches]) for decomposed domains
+ integer(i4b),  intent(out)               :: ierr            ! error code
+ character(*),  intent(out)               :: message         ! error message
  ! Local variables
  logical(lgt)                             :: verbose        ! check details of variables
  real(dp)                                 :: q_upstream     ! total discharge at top of the reach being processed
