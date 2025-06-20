c86bee9a462ca175be01784b71b7aa7fd557d0fa
Naoki <mizukami@ucar.edu>
2025-06-20 07:46:58 -0600

    try adding equation numbering config. take 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,16 +40,10 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
 ]
 
-math_number_all = True
-math_eqref_format = "({number})"
-
-latex_elements = {
-    'numbered_equations': True,
-    'equation_numbering': 'bysection',
-}
+math_numfig = True
+numfig = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
